9d1328d0694b3e697255265590fa8e1a8088a9cb
Anmol <anmolraikwar2006@gmail.com>
2025-04-15 15:46:08 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,8 @@ import type { Config } from "tailwindcss";
 export default {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
+		"./pages/**/*.{js,ts,tsx,jsx}",
+		"./components/**/*.{js,ts,tsx,jsx}",
 		"./app/**/*.{ts,tsx}",
 		"./src/**/*.{ts,tsx}",
 	],
